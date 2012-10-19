1a40208ee943b554daee04398f5e0b011f198378
Sam Bleckley <diiq.diiq@gmail.com>
2012-10-19 23:10:49 +1300

    Works now with partially-columnar text; will narrow while you edit.

--- multi-column-mode.el
+++ multi-column-mode.el
@@ -18,6 +18,7 @@
   :prefix "multi-column-"
   :group 'frames)
 
+(defvar multi-column-width -1)
 
 (defun multi-column-fill-goto-column (column)
   "Goto column, and fill with spaces if the line is too short."
@@ -66,39 +67,68 @@ window-walked for the order of windows to examine."
 
 (defun multi-column-longest-line ()
   "Returns the length of the longest line in the buffer."
-  (apply 'max (multi-column-map-lines '(lambda () (end-of-line) (current-column)))))
+  (apply 'max (multi-column-map-lines 
+               '(lambda () (end-of-line) (current-column)))))
 
 
+(defun multi-column-end-break (direction)
+  "Finds the vertical end of a break between horizontal columns,
+if the break consists of identical characters."
+  (save-excursion
+    (let ((divider (char-after))
+          (col (current-column))
+          (test 't))
+      (while (and test
+                  (or (< (line-end-position)
+                    (+ col (line-beginning-position)))
+                 (eq (char-after
+                      (+ col (line-beginning-position)))
+                     divider)))
+        (setq test (eq 0 (forward-line direction))))
+      (if (> 0 direction) (forward-line))
+      (line-beginning-position))))
 
+(defun multi-column-narrow ()
+  (let ((start (multi-column-end-break -1))
+        (end (multi-column-end-break 1)))
+    (narrow-to-region start end)))
 
 (defun multi-column-split ()
   "Split horizontally, at point, into two buffers."
   (interactive)
+  (multi-column-narrow)
+  (delete-trailing-whitespace)
   (save-excursion ; Won't work, have to roll own based on line number.
     (let* ((width (current-column))
            (new-buffer (generate-new-buffer "column"))
            (new-column (split-window-horizontally (+ 4 width))))
+      (set (make-local-variable 'multi-column-width) width)
       (set-fill-column width)
       (setq multi-column-width width)
       (set-window-buffer new-column new-buffer)
       (multi-column-shift-column width (current-buffer) new-buffer)
-      (set-buffer new-buffer) (multi-column-mode)
-      (setq multi-column-width 10000))))
+      (set-buffer new-buffer) (multi-column-mode))))
 
 (defun multi-column-merge (left-window right-window)
   "Merge two windows into one, splicing the buffers horizontally."
+  ; What is this imperative nonsense? Goodness me.
   (set-buffer (window-buffer right-window))
+  (delete-trailing-whitespace)
   (goto-char (point-max))
   (multi-column-fill-goto-column (multi-column-longest-line))
   (let ((right-rect (delete-extract-rectangle (point-min) (point-max))))
     (set-buffer (window-buffer left-window))
+    (delete-trailing-whitespace)
     (goto-char (point-min))
-    (multi-column-fill-goto-column (multi-column-longest-line))
+    (multi-column-fill-goto-column (max (multi-column-longest-line) multi-column-width))
     (insert-rectangle right-rect)
     (kill-buffer (window-buffer right-window))
     (delete-window right-window)
     (select-window left-window)
-    (setq multi-column-width (multi-column-longest-line))))
+    (delete-trailing-whitespace)
+    (widen)
+    (setq multi-column-width (multi-column-longest-line)))) 
+                                        ; Some better value exists for this.
     
 (defun multi-column-merge-left ()
   "Merge this column with the next multi-column-mode window to the left."
